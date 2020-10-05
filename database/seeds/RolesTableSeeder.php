aa4c0ea386a7a2a1f913b7153f792b4e00a4172a
shubham agnihotri <shubham.a@coppermobile@gmail.com>
2020-10-05 17:18:04 +0530

    role seed bug fix

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -14,8 +14,8 @@ class RolesTableSeeder extends Seeder
     {
         DB::table('roles')->delete();
         DB::table('roles')->insert([
-            ['id' => 1, 'role' => 'NTTPC Super Admin',  'role_description' => 'nttpc_super_admin'],
-            ['id' => 2, 'role' => 'SuiteX/S Admin',    'role_description' => 'suitexs_admin'],
+            ['id' => 1, 'role_name' => 'admin'],
+            ['id' => 2, 'role_name' => 'user'],
         ]);
     }
 }
